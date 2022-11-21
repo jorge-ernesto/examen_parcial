7f68c5299397b73df05e704f08fe7dd6dc5b5611
Ernesto <jorge.cywdt@gmail.com>
2022-11-21 05:46:24 -0500

    Crear modelo 'Tarea'

--- gestion_tareas/models.py
+++ gestion_tareas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Titulo")
+    description = models.TextField(verbose_name="Descripcion")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado el")
+    delivery_at = models.DateTimeField(verbose_name="Entregado el")
+
+    class Meta:
+        verbose_name = "Tarea"
+        verbose_name_plural = "Tareas"
+
+    def __str__(self):
+        return self.title
